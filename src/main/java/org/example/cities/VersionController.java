259368e930fbd805fd588afef17415a95f0c5941
unknown <rpand9@BLRRPAND952635.sapient.com>
2014-12-03 17:12:53 +0530

    change version number

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Paris Roadshow!";
+        return "Welcome to Pivotal Roadshow!";
     }
 }
